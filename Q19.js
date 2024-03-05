cdb43a0e460e695a0c397c89003946dcb9b6835e
Muhammad-Hassan99 <162054676+Muhammad-Hassan99@users.noreply.github.com>
2024-03-06 01:28:22 +0500

    Add files via upload

---
+++ Q19.js
@@ -0,0 +1,8 @@
+"use strict";
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+let list1 = ["Farhan", "Zayan", "Ayan"];
+// console.log( list[0] +" I would like to invite you on a dinner ")
+// console.log( list[1] +" I would like to invite you on a dinner ")
+// console.log( list[2] +" I would like to invite you on a dinner ")
+let lengthguest = list1.length;
+console.log(`We are inviting ${lengthguest} guest`);
