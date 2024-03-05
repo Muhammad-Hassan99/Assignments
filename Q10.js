11cad85d1a3c26528a1cf66e3a6f5dd475e9f9b2
Muhammad-Hassan99 <162054676+Muhammad-Hassan99@users.noreply.github.com>
2024-03-06 01:30:14 +0500

    Add files via upload

---
+++ Q10.js
@@ -0,0 +1,13 @@
+"use strict";
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+// store a values in a variable then i perform following operations
+let l = 2;
+let m = 16;
+//multiplication//
+console.log(l * m);
+//addition//
+console.log(m + l);
+//subtraction//
+console.log(m - l);
+//division//
+console.log(m / l);
---
+++ Q12.ts
@@ -0,0 +1,5 @@
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let h=["Hassan", "Farhan" , "Zayan"]
+console.log(h[0]+ " is my friend")
+console.log(h[1]+ " is my friend")
+console.log(h[2]+ " is my friend")
