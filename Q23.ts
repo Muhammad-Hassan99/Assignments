9d227c415ac63d81b0be7000a07acc954cc38d72
Muhammad-Hassan99 <162054676+Muhammad-Hassan99@users.noreply.github.com>
2024-03-21 03:39:54 +0500

    Add files via upload

---
+++ Q23.ts
@@ -0,0 +1,27 @@
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+// let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True.")
+
+// console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+let y=25
+console.log(y==25)
+console.log(y==23)
+
+let bike="honda"
+console.log(bike == "honda")
+console.log(bike == "Unique")
+let num1:any=23
+console.log(num1 === 23)
+console.log(num1 === "23")
+let fruit="mango"
+console.log(fruit == "mango")
+console.log(fruit=="banana")
+let num2:any=9
+console.log(num2===9)
+console.log(num2==="9")
---
+++ Q27.js
@@ -0,0 +1,36 @@
+"use strict";
+// Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+// • If the alien is green, print a message that the player earned 5 points.
+// • If the alien is yellow, print a message that the player earned 10 points.
+// • If the alien is red, print a message that the player earned 15 points.
+// • Write three versions of this program, making sure each message is printed for the appropriate color alien.
+let aliencolour1 = "Green";
+if (aliencolour1 === "Green") {
+    console.log("You just earned 5 points");
+}
+else if (aliencolour1 === "Yellow") {
+    console.log("You just earned 10 points");
+}
+else if (aliencolour1 === "Red") {
+    console.log("You just earned 15 points");
+}
+let aliencolour2 = "Yellow";
+if (aliencolour2 === "Green") {
+    console.log("You just earned 5 points");
+}
+else if (aliencolour2 === "Yellow") {
+    console.log("You just earned 10 points");
+}
+else if (aliencolour2 === "Red") {
+    console.log("You just earned 15 points");
+}
+let aliencolour3 = "Red";
+if (aliencolour3 === "Green") {
+    console.log("You just earned 5 points");
+}
+else if (aliencolour3 === "Yellow") {
+    console.log("You just earned 10 points");
+}
+else if (aliencolour3 === "Red") {
+    console.log("You just earned 15 points");
+}
